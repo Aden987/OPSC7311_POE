6949f95330d4b5a3d27193c95dd7d49669dd0328
Michael Stewart <michaelandrewstew@gmail.com>
2023-09-25 14:58:19 +0200

    Added the Login and Selection Page
    Added the login and selection page with corresponding files

--- app/src/main/java/com/example/opsc_poe/MainActivity.kt
+++ app/src/main/java/com/example/opsc_poe/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.opsc_poe
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +14,35 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.opsc_poe.databinding.LoginBinding
 import com.example.opsc_poe.ui.theme.OPSC_POETheme
 
+
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: LoginBinding
+
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var  loginButton : Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            OPSC_POETheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+        binding = LoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if(binding.username.text.toString() == "user" && binding.password.text.toString() == "Password@1"){
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                setContentView(R.layout.selection_page)
             }
-        }
-    }
-}
+            else
+            {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+            }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OPSC_POETheme {
-        Greeting("Android")
+        })
     }
 }
